3c94bc502d650caa6dd9e8eb913187c12ec255dd
felix <felix.hilden@gmail.com>
2020-06-06 17:09:04 +0300

    Add tools and documentation template

--- readme.rst
+++ readme.rst
@@ -1,3 +1,17 @@
-python-minimal
+python-package
 ==============
-A minimal package structure for Python to clone when creating a new repository.
+A package template for Python.
+
+Assumed tooling
+---------------
+- Hosted on GitHub with a Wiki
+- Pytest and Coverage for testing
+- Tox and linters for static analysis
+- Numpy-style ``.rst`` documentation with Sphinx (see ``docs/src/conf.py``)
+
+Considerations
+--------------
+- Move documentation to ReadTheDocs:
+  make a ``404.rst`` file under ``docs/src`` and
+  require specific doc package versions in ``docs/requirements.txt``
+- Use continuous integration like GitHub Actions or Travis
--- setup.py
+++ setup.py
@@ -12,15 +12,47 @@ root = Path(os.path.realpath(__file__)).parent
 version_file = root / 'package' / 'VERSION'
 readme_file = root / 'readme.rst'
 
+pypi_url = 'https://pypi.org/project/python-package'
+github_url = 'https://github.com/felix-hilden/python-package'
+documentation_url = github_url + '/wiki'
+
+extras_require = {
+    'docs': [
+        'sphinx',
+        'sphinx-rtd-theme',
+        'sphinx-autodoc-typehints'
+    ],
+    'tests': [
+        'coverage',
+        'pytest',
+    ],
+    'checks': [
+        'tox',
+        'doc8',
+        'flake8',
+        'flake8-bugbear',
+        'pydocstyle',
+        'pygments',
+    ]
+}
+extras_require['dev'] = (
+    extras_require['docs'] + extras_require['tests'] + extras_require['checks']
+)
+
 setuptools.setup(
-    name='python-minimal',
+    name='python-package',
     version=version_file.read_text().strip(),
-    description='minimal python package template',
+    description='Python package template',
     long_description=readme_file.read_text(),
     long_description_content_type='text/x-rst',
 
-    url='https://github.com/felix-hilden/python-minimal',
-    download_url='https://github.com/felix-hilden/python-minimal',
+    url=documentation_url,
+    download_url=pypi_url,
+    project_urls={
+        'Source': github_url,
+        'Issues': github_url + '/issues',
+        'Documentation': documentation_url,
+    },
 
     author='Felix Hildén',
     author_email='felix.hilden@gmail.com',
@@ -28,7 +60,8 @@ setuptools.setup(
     maintainer_email='felix.hilden@gmail.com',
 
     license='MIT',
-    packages=setuptools.find_packages(),
+    keywords='python package template',
+    packages=setuptools.find_packages(exclude=('tests', 'tests.*',)),
     include_package_data=True,
     package_data={
         'package': ['VERSION']
@@ -36,4 +69,7 @@ setuptools.setup(
 
     python_requires='>=3.6',
     install_requires=[],
+    extras_require=extras_require,
+
+    classifiers=[],
 )
